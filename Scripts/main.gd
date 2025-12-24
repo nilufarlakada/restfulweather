e34813abe8e45d55019daded2350b722bcb94962
Nilufar Lakada <nilufarlakada@gmail.com>
2025-12-23 19:31:46 -0600

    snowing scene made

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,12 +1,12 @@
 class_name MainClass extends Node3D
 
-var url = "https://api.openweathermap.org/data/2.5/weather?lat=41.842329&lon=-447.642481&appid=7bd174330b5d9b877ba1b48cce113055"
+var url = "https://api.openweathermap.org/data/2.5/weather?lat=41.84238640042879&lon=-87.64249600576402&appid=7bd174330b5d9b877ba1b48cce113055&units=imperial"
 #var API_KEY = "7bd174330b5d9b877ba1b48cce113055"
 @onready var http_request = $HTTPRequest
-
-#func _ready()-> void:
-	#http_request.request_completed.connect(_on_request_completed)
-	#send_request()
+#41.84238640042879, -87.64249600576402
+func _ready()-> void:
+	http_request.request_completed.connect(_on_request_completed)
+	send_request()
 
 func send_request():
 	var headers = ["Content-Type: application/json"]
